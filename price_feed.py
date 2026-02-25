3b61c54eb6d47f27cf46d7edbd637b062081e7ba
leonardoans07 <leodabet33@gmail.com>
2026-02-25 18:59:01 -0300

    Create price_feed.py

---
+++ price_feed.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+def get_last_price(symbol: str) -> Optional[float]:
+    """
+    Retorne o último preço (mid ou last) do símbolo.
+    IMPORTANTÍSSIMO: aqui você cola o método que vocês já usam hoje.
+    """
+    # TODO: cole aqui a sua função real (ex: TwelveData/FXCM/MetaTrader bridge/etc)
+    return None
